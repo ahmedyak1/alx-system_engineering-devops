7a2fc518ea397e11d8aae9c82076909b99ee04c5
ahmedyak1 <ahmedyakine1@gmail.com>
2024-01-01 17:25:55 -0800

    task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#  configura SSH config file for autom passw connec using Puppet
+
+include stdlib
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
+
